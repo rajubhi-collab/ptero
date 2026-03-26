51c8fa482553da6e6d060b32a778e20aa3f3d77b
rajubhi-collab <rajbhai00797@gmail.com>
2026-03-26 21:15:45 +0530

    Update dev.sh

--- ptero/main/dev.sh
+++ ptero/main/dev.sh
@@ -77,7 +77,7 @@ while true; do
         5) bash <(curl -s https://raw.githubusercontent.com/rajubhi-collab/ptero/refs/heads/main/ptero/thame/chang/dev.sh) ;;
         6) bash <(curl -s https://raw.githubusercontent.com/nobita329/The-Coding-Hub/refs/heads/main/srv/menu/System1.sh) ;;
         7) bash <(curl -s https://raw.githubusercontent.com/rajubhi-collab/ptero/refs/heads/main/ptero/no-kvm/run.sh) ;;
-        8) bash <(curl -fsSL https://bit.ly/rajbhaicmd)
+        8) bash <(curl -fsSL https://bit.ly/rajbhaicmd) ;;
         9|exit) 
             echo -e "\n  ${B_RED}DISCONNECTED.${NC} Goodbye, RAJBHAI."
             exit 0 ;;
